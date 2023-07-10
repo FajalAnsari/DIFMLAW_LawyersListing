10760fd79c2a3e081cd0809b5d7dc74caaa96fcb
bobyswaroop <bobyswaroop555024@gmail.com>
2023-07-10 17:27:03 +0530

    review section on working

--- src/components/job_description_page/Add_review/Add_review.jsx
+++ src/components/job_description_page/Add_review/Add_review.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
-
+import "./Add_review.css";
 const Add_review = () => {
   return (
     <>
-    <div className='container'>
-      <div className=''>
+    <div className='mt-5 '>
+      <div className='review p-4'>
         <div className='d-flex'>
-         <p className='text-white'>Write A Review</p>
-         
+        <div><p className='text-white'>Write A Review</p></div>
+        <div className='float-right'><p className='fs-1 font-color'><i class="bi bi-caret-down-fill"></i></p></div>
+
+        </div>
+        <div className='row'>
+          <div className='col-lg-6'>
+            <div className='row'>
+              <div className='col-lg-6'>
+                <p>Your Rating</p>
+              </div>
+              <div className='col-lg-6'>
+              <i class="bi bi-star"></i>
+              </div>
+            </div>
+          </div>
+          <div className='col-lg-6'>
+
+          </div>
         </div>
       </div>
     </div>
