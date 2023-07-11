39b2ca6bc5b3947da4f3461e12c142eb7f538e74
usha singh <ushasing215105@gmail.com>
2023-07-11 09:40:06 +0530

    complete add comment section

--- src/components/job_description_page/Add_review/Add_Comment.jsx
+++ src/components/job_description_page/Add_review/Add_Comment.jsx
@@ -1,35 +1,42 @@
 import React from 'react';
+import { dummy } from '../../images';
+import "./review_section.css";
 
 const Add_Comment = () => {
   return (
     <>
-    <div className="add-comment bg-secondary py-4 px-4 rounded-3  border border-prime border-1">
+    <div className="add-comment py-4 px-4 rounded-3  border border-prime border-1" style={{backgroundColor:"var(--fourth-primary)"}}>
       <div className="row">
         <div className="col-md-2">
-          <img src="https://www.dlf.pt/dfpng/middlepng/569-5693658_dummy-user-image-png-transparent-png.png" alt="" className='w-75 rounded-circle'/>
+          <img src={dummy} alt="" className='w-100 rounded-circle'/>
         </div>
         <div className="col-md-10">
             <div className="row">
                 <div className="col-md-8">
-                  <p className='fs-5 font-color'>Fajal Ansari<br />June 2, 2023</p>
+                  <p className='fs-6' style={{color:"var(--third-secondary)"}}>Fajal Ansari<br />June 2, 2023</p>
                 </div>
                 <div className="col-md-4">
-                 <div className="btns-primary px-2 w-50">
-                 <i class="bi bi-star"></i>
-                 <i class="bi bi-star"></i>
-                 <i class="bi bi-star"></i>
-                 <i class="bi bi-star"></i>
-                 <i class="bi bi-star"></i>
-                 {/* <i><p>5.0</p></i> */}
+                 <div className="btns-primary d-flex px-1 w-50" style={{height:"23px"}}>
+                 <i class="bi bi-star me-1"></i>
+                 <i class="bi bi-star me-1"></i>
+                 <i class="bi bi-star me-1"></i>
+                 <i class="bi bi-star me-1"></i>
+                 <i class="bi bi-star me-1"></i>
+                  <div className=' px-2 ' style={{height:"23px",backgroundColor:"var(--third-secondary)"}}>
+                    <p className='fs-6 fw-bold font-color' style={{marginTop:"-2px"}}>5.0</p>
+                  </div>
                  </div>
                 </div>
-                <p className='text-white' style={{fontSize:"13px"}}>
+                <p style={{fontSize:"13px",color:"var(--text-color)"}}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat nunc ac a magna at elementum. Cras arcu varius in aliquam habitant fermentum. Mi sit lorem mollis vitae quis curabitur vestibulum.<br/><br/>
 
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat nunc ac a magna at elementum. Cras arcu varius in aliquam habitant fermentum. Mi sit lorem mollis vitae quis curabitur vestibulum.<br/><br/>
 
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat nunc ac a magna at elementum. Cras arcu varius in aliquam habitant fermentum. Mi sit lorem mollis vitae quis curabitur vestibulum.
                 </p>
+                <hr
+              className="mb-1 ms-2 add_com"
+            />
             </div>
         </div>
       </div>
