a0c55d710e957e808a8fa49368126d5e58ff23f9
usha singh <ushasing215105@gmail.com>
2023-07-12 10:08:03 +0530

    bookmark function add

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -1,41 +1,88 @@
 import React,{useState, useEffect} from 'react';
 import { auth } from '../../firebase';
 import { db } from '../../firebase';
-import Add_Lawyercart from './Add_Lawyercart';
+import { collection, getDocs, query } from 'firebase/firestore';
+import { onAuthStateChanged } from 'firebase/auth';
+
 
 const Bookmark = () => {
 
-     // state of cart products
-     const [add_Lawyercarts, setAdd_Lawyercarts]=useState([]);
-
      // getting cart products from firestore collection and updating the state
-     useEffect(()=>{
-         auth.onAuthStateChanged(user=>{
-             if(user){
-                 db.collection('Cart ' + user.uid).onSnapshot(snapshot=>{
-                     const newCartProduct = snapshot.docs.map((doc)=>({
-                         ID: doc.id,
-                         ...doc.data(),
-                     }));
-                     Add_Lawyercart(newCartProduct);                    
-                 })
-             }
-             else{
-                 console.log('user is not signed in to retrieve cart');
-             }
-         })
-     },[])
+     const [add_Lawyercarts, setAdd_Lawyercarts] = useState([]);
+
+     useEffect(() => {
+       const unsubscribe = auth.onAuthStateChanged(async (user) => {
+         if (user) {
+           const cartRef = collection(db, `Cart ${user.uid}`);
+           const querySnapshot = await getDocs(cartRef);
+   
+           const newCartProduct = querySnapshot.docs.map((doc) => ({
+             id: doc.id,
+             image: doc.image || '', 
+             ...doc.data(),
+           }));
+           setAdd_Lawyercarts(newCartProduct);
+         } else {
+           console.log('User is not signed in to retrieve cart');
+         }
+       });
+   
+       return () => {
+         // Unsubscribe from the auth state listener when the component unmounts
+         unsubscribe();
+       };
+     }, []);
+
+     console.log(add_Lawyercarts);
+  
      
   return (
     <>
       {add_Lawyercarts.length > 0 && (
         <div className='container'>
-          <Add_Lawyercart />
+           <div className="row mx-auto lawyers_profile">
+           {add_Lawyercarts.map((add_Lawyercart, i) => (
+      <div className="col-lg-3 rounded-3 lawyer ecard mt-4  card shadow p-3 mb-5 bg-body rounded lawyers-card" key={i}>
+        <div className="row  mt-2" id="lawyer">
+          <div className="col-lg-4 col-sm-4 col-6">
+            <img src={add_Lawyercart.image} className="ms-2 lawpics" alt='lawyer_card'></img>
+          </div>
+          <div className="col-lg-8 col-sm-8 col-6">
+            <p className="fs-6 mb-0 pb-1 h6">{add_Lawyercart.username}</p>
+            <p className="city">{add_Lawyercart.address}</p>
+          </div>
+        </div>
+
+        <span className="fs-5 fw-normal text-center">{add_Lawyercart.specialization}</span>
+
+        <div className="mt-3 ms-3 me-3">
+          <p className="font-weight-bold fs-6 mb-1">{add_Lawyercart.work}</p>
+          <p className="fs-6 lawyers-desc font-weight-normal lh-base text-justify summ">
+            {add_Lawyercart.summary}
+          </p>
+          <div className="row mt-4 practice">
+            <div className="col-lg-6 col-sm-6 col-6">
+              <span className="fs-6 exp">{add_Lawyercart.experience} in practice</span>
+            </div>
+            <div className="col-lg-6 col-sm-6 col-6">
+              <button className="btn btns-primary sfcs viewbtn">
+                View Profile
+              </button>
+              {/* <div className="col-md-1 mx-3 res4">
+          <i class="bi bi-bookmark fw-bold fs-3"></i>
+          <p className='fs-6 savelist'>save</p>
+          </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+       ))}
+    </div>
         </div>
       )}
       {add_Lawyercarts.length < 1 && (
         <div className='container'>
-         No Data SHow
+         <p className='text-white'>No Data SHow</p>
         </div>
       )}
     </>
