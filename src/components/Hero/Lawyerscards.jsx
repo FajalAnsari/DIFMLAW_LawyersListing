996c17f5b2dfed6026e0a538e38a139c9816eb78
bobyswaroop <bobyswaroop555024@gmail.com>
2023-07-13 08:55:42 +0530

    pull

--- src/components/Hero/Lawyerscards.jsx
+++ src/components/Hero/Lawyerscards.jsx
@@ -32,6 +32,31 @@ const Lawyerscards = () => {
         <div className="row mx-auto lawyers_profile">
           {[...Array(8)].map((_, i) => (
            <div className="col-lg-3 rounded-3 lawyer ecard mt-4 card shadow p-3 mb-5 bg-body rounded lawyers-card" key={i}>
+           <div className="row mt-2" id="lawyer">
+                <div className="col-lg-4 col-sm-4 col-6">
+                  <div className="ms-2 lawpics" style={{backgroundColor:'var(--third-primary)'}} alt="lawyer_card"></div>
+                </div>
+                <div className="col-lg-8 col-sm-8 col-6">
+                  <p className="fs-6 mb-0 pb-1 h6" style={{backgroundColor:'var(--third-primary)'}}></p>
+                  <p className="city" style={{backgroundColor:'var(--third-primary)'}}></p>
+                </div>
+              </div>
+              <span className="fs-5 fw-normal text-center"></span>
+
+<div className="mt-3 ms-3 me-3">
+  <p className="font-weight-bold fs-6 mb-1"></p>
+  <p className="fs-6 lawyers-desc font-weight-normal lh-base text-justify summ"></p>
+  <div className="row mt-4 practice">
+    <div className="col-lg-6 col-sm-6 col-6">
+      <span className="fs-6 exp"> in practice</span>
+    </div>
+    <div className="col-lg-6 col-sm-6 col-6">
+      <button className="btn btns-primary sfcs viewbtn">
+        View Profile
+      </button>
+    </div>
+  </div>
+</div>
            <div style={{ height: 400 }}>
             <div className='preloader-img'></div>
              <Skeleton height={400} />
