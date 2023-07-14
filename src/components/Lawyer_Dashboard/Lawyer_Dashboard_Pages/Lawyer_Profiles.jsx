cb2c9e60160635a50af4db5c9c9ba1600aab1b51
usha singh <ushasing215105@gmail.com>
2023-07-14 16:34:46 +0530

    upadte

--- src/components/Lawyer_Dashboard/Lawyer_Dashboard_Pages/Lawyer_Profiles.jsx
+++ src/components/Lawyer_Dashboard/Lawyer_Dashboard_Pages/Lawyer_Profiles.jsx
@@ -11,7 +11,7 @@ const Lawyer_Profiles = () => {
           <div className="col-md-4">
              <div className='user_pro'>
                  <div className="d-flex px-4 border border-prime border-4 rounded-full  um">
-                   <img src={dummy} alt="dummy" className='' style={{width:"225%",height:"100%",marginLeft:"-24px"}}/>
+                   <img src={dummy} alt="dummy" className='umm' style={{width:"225%",height:"100%",marginLeft:"-24px"}}/>
                  </div>
                  
               </div>
@@ -23,75 +23,75 @@ const Lawyer_Profiles = () => {
                     <form>
                         {/* <!-- Form Row--> */}
                         <div className="row gx-3 mb-1">
-                            <div className="col-md-6">
-                                <label className="small mb-1 text-white" for="inputFirstName">Full Name</label>
-                                <div class="input-group mb-3">
+                            <div className="col-md-6 mbs1">
+                                <label className="small mb-1 text-white " for="inputFirstName">Full Name</label>
+                                <div class="input-group mb-3 mbs">
                                  <input className="form-control contect-bgColors" id="inputFirstName" type="text"/>
-                                 <span class="input-group-text btns-primary border-prime bolder"><i class="bi bi-person"></i></span>
+                                 <span class="input-group-text btns-primary border-prime bolder dm"><i class="bi bi-person"></i></span>
                                </div>
                             </div>
-                            <div className="col-md-6">
+                            <div className="col-md-6 mbs1">
                                 <label className="small mb-1 text-white" for="inputEmail">Email</label>
-                                <div class="input-group mb-3">
-                                <input className="form-control contect-bgColors" id="inputEmail" type="email"  disabled/>
-                                 <span class="input-group-text btns-primary border-prime" ><i class="bi bi-envelope-open"></i></span>
+                                <div class="input-group mb-3 mbs">
+                                <input className="form-control contect-bgColors dm" id="inputEmail" type="email"  disabled/>
+                                 <span class="input-group-text btns-primary border-prime dm" ><i class="bi bi-envelope-open"></i></span>
                                </div>
                             </div>
                         </div>
                         <div className="row gx-3 mb-1">
-                        <div className="col-md-6">
+                        <div className="col-md-6 mbs1">
                                 <label className="small mb-1 text-white" for="inputMobile">Mobile</label> 
-                                <div class="input-group mb-3">
-                                  <input className="form-control contect-bgColors" id="inputMobile" type="tel"/>
-                                  <span class="input-group-text btns-primary border-prime" ><i class="bi bi-telephone"></i></span>
+                                <div class="input-group mb-3 mbs">
+                                  <input className="form-control contect-bgColors dm" id="inputMobile" type="tel"/>
+                                  <span class="input-group-text btns-primary border-prime dm" ><i class="bi bi-telephone"></i></span>
                                 </div>    
                             </div>
                             
-                            <div className="col-md-6">
+                            <div className="col-md-6 mbs1">
                                 <label className="small mb-1 text-white" for="inputExperience">Experience</label>    
-                                <div class="input-group mb-3">
+                                <div class="input-group mb-3 mbs">
                                 <input className="form-control contect-bgColors" id="inputExperience" type="text"/>
-                                <span class="input-group-text btns-primary border-prime" ><i class="bi bi-briefcase-fill"></i></span>
+                                <span class="input-group-text btns-primary border-prime dm" ><i class="bi bi-briefcase-fill"></i></span>
                               </div>
                             </div>
                         </div>
                        
                         <div className="row gx-3 mb-1">
-                            <div className="col-md-6">
+                            <div className="col-md-6 mbs1">
                                 <label className="small mb-1 text-white" for="inputSpecialization">Specialization</label>
-                                <div class="input-group mb-3">
+                                <div class="input-group mb-3 mbs">
                                 <input className="form-control contect-bgColors" id="inputSpecialization" type="text"/>
-                                <span class="input-group-text btns-primary border-prime" ><i class="bi bi-person-fill"></i></span>
+                                <span class="input-group-text btns-primary border-prime dm" ><i class="bi bi-person-fill"></i></span>
                               </div>
                             
                             </div>
                            
-                            <div className="col-md-6">
+                            <div className="col-md-6 mbs1">
                                 <label className="small mb-1 text-white" for="inputEducation">Education</label>   
-                                <div class="input-group mb-3">
+                                <div class="input-group mb-3 mbs">
                                 <input className="form-control contect-bgColors" id="inputEducation" type="text" name="education" placeholder="Describe your education qualifications"  required/>
-                                  <span class="input-group-text btns-primary border-prime" ><i class="bi bi-person-fill"></i></span>
+                                  <span class="input-group-text btns-primary border-prime dm" ><i class="bi bi-person-fill"></i></span>
                                 </div>
                             </div>
                         </div>
                         <div className="row gx-3 mb-3">
-                            <div className="col-md-6">
+                            <div className="col-md-6 mbs1">
                                 <label className="small mb-1 text-white" for="inputWork">Work Type</label>
-                                <div class="input-group mb-3">
+                                <div class="input-group mb-3 mbs">
                                 <input className="form-control contect-bgColors" id="inputWor" type="text"/>
-                                <span class="input-group-text btns-primary border-prime" ><i class="bi bi-person-workspace"></i></span>
+                                <span class="input-group-text btns-primary border-prime dm" ><i class="bi bi-person-workspace"></i></span>
                               </div>
                             </div>
                            
-                            <div className="col-md-6">
+                            <div className="col-md-6 mbs1">
                             <label className="small mb-1 text-white" for="inputWork">Enter or choose location</label>
-                            <div class="input-group mb-3">
+                            <div class="input-group mb-3 mbs">
                              <input type="text" class="form-control contect-bgColors" id="inputZip" placeholder="Location, country, city, state..." />
-                             <span class="input-group-text btns-primary" ><i class="bi bi-geo-alt"></i></span>
+                             <span class="input-group-text btns-primary dm" ><i class="bi bi-geo-alt"></i></span>
                            </div>
                             </div>
                         </div>
-                        <div class="col-md-12">
+                        <div class="col-md-12 mbs1">
                         <label for="inputZip" class="form-label text-white">Bio/Profile Summary</label>
                         <textarea class="form-control ed text-primary contect-bgColors" maxLength="250"  id="form6Example7" rows="4" ></textarea>
                         </div>
